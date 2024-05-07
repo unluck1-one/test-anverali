47add5756ea860c284593b3a6729123e89bba1b2
unluck1-one <113229682+unluck1-one@users.noreply.github.com>
2024-05-07 19:30:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-api = "6711144869:AAFZ3cIuIZQBjgwbkYiO4acoOmUn9pq_kCQ"
-conDet = "user=postgres password=k0525k78 host=localhost port=5432 dbname=postgres" # здесь нужно поменять данные от БД на ваши
+api = "YOUR_TOKEN" # вместо YOUR_TOKEN вставьте ваш токен из BotFather
+conDet = "user=postgres password=YOUR_PASSWORD host=localhost port=5432 dbname=postgres" # здесь нужно поменять данные от БД на ваши и password
